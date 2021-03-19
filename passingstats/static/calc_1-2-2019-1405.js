5163eb8d67c6e6acd88c9b24e3515785da8f66f5
Cody J. King <codytheking93@gmail.com>
2021-03-19 13:39:35 -0700

    Add passingstats page

---
+++ passingstats/static/calc_1-2-2019-1405.js
@@ -0,0 +1,50 @@
+console.log("Hey there! Thanks for using the app!");
+
+var total = [0, 0, 0, 0, 0, 0];
+var numVals = [0, 0, 0, 0, 0, 0];
+var count = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]];
+
+function avg(number, player)
+{
+    var index = player;
+
+    var error = false;
+    total[index] += number;
+    numVals[index]++;
+    count[index][number]++;
+    
+    updateText(index);
+}
+
+function updateText(player)
+{
+    var index = player;
+    // print result to screen
+    document.getElementById("answer" + index).innerHTML = "Current average (" +
+        numVals[index] + "): " + Math.round((total[index] / numVals[index]) * 100) / 100;
+
+    document.getElementById("zeros" + index).innerHTML = "\nZeros: " + count[index][0];
+    document.getElementById("ones" + index).innerHTML = "Ones: " + count[index][1];
+    document.getElementById("twos" + index).innerHTML = "Twos: " + count[index][2];
+    document.getElementById("threes" + index).innerHTML = "Threes: " + count[index][3];
+}
+
+function resetTotal(player)
+{
+    var index = player;
+
+    total[index] = 0;
+    numVals[index] = 0;
+
+    document.getElementById("answer" + index).innerHTML = "";
+    document.getElementById("zeros" + index).innerHTML = "";
+    document.getElementById("ones" + index).innerHTML = "";
+    document.getElementById("twos" + index).innerHTML = "";
+    document.getElementById("threes" + index).innerHTML = "";
+
+    for(var i = 0; i < count[index].length; i++)
+    {
+        count[index][i] = 0;
+    }
+}
+
